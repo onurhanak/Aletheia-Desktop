4a61ecae10009e1c481e2f3cf26c86a3cfdf34ef
Onurhan Ak <onurhanak@protonmail.com>
2022-08-18 20:28:23 +0300

    added theme support

--- services/saveSettings.js
+++ services/saveSettings.js
@@ -1,46 +1,71 @@
-const fs = require('fs')
+const fs = require("fs");
 
-const homedir = require('os').homedir();
-var dir = '/.LibgenDesktop/'
-var settingsFile='settings.json'
-var downloadPath = homedir+dir
-var settingsPath = downloadPath+settingsFile
+const homedir = require("os").homedir();
+var dir = "/.LibgenDesktop/";
+var settingsFile = "settings.json";
+var downloadPath = homedir + dir;
+var settingsPath = downloadPath + settingsFile;
 
 function setSettings(mirror, theme) {
-    fs.closeSync(fs.openSync(settingsPath, 'a'))
-    let rawSettings = fs.readFileSync(settingsPath);
-    let jsonSettings = JSON.parse(rawSettings)
-    if (mirror!=undefined) {
-        jsonSettings['mirror'] = mirror
-    } 
-    if (theme != undefined) {
-        jsonSettings['theme'] = theme
-        applyTheme(theme)
-    }
-    let data = JSON.stringify(jsonSettings)
-    fs.writeFileSync(settingsPath, data);
+  fs.closeSync(fs.openSync(settingsPath, "a"));
+  let rawSettings = fs.readFileSync(settingsPath);
+  let jsonSettings = JSON.parse(rawSettings);
+  if (mirror != undefined) {
+    jsonSettings["mirror"] = mirror;
+  }
+  if (theme != undefined) {
+    jsonSettings["theme"] = theme;
+    applyTheme(theme);
+  }
+  let data = JSON.stringify(jsonSettings);
+  fs.writeFileSync(settingsPath, data);
 }
 
 function readSettings() {
-    let rawSettings = fs.readFileSync(settingsPath);
-    let jsonSettings = JSON.parse(rawSettings)
-    return [jsonSettings['theme'],jsonSettings['mirror']]
+  let rawSettings = fs.readFileSync(settingsPath);
+  let jsonSettings = JSON.parse(rawSettings);
+  return [jsonSettings["theme"], jsonSettings["mirror"]];
 }
 
-function applyTheme(theme){
-    var views = ['index.css','library.css','search.css','settings.css']
-    if (theme=='dark'){
-        for (let view of views) {
-            var cssFile = fs.readFileSync(view);
-            var newCssFile = cssFile.replace("@import 'theme-light.css'","@import 'theme-dark.css'")
-            fs.writeFileSync(view,newCssFile)
+function applyTheme(theme) {
+  var views = ["index.css", "library.css", "search.css", "settings.css"];
+  if (theme == "dark") {
+    for (let view of views) {
+      view = "public/stylesheets/" + view;
+
+      fs.readFile(view, "utf8", function (err, data) {
+        if (err) {
+          return console.log(err);
         }
-        
+        var result = data.replace(
+          "@import 'theme-light.css'",
+          "@import 'theme-dark.css'"
+        );
 
-    } else if (theme=='light') {
-
+        fs.writeFile(view, result, "utf8", function (err) {
+          if (err) return console.log(err);
+        });
+      });
     }
+  } else if (theme == "light") {
+    for (let view of views) {
+      view = "public/stylesheets/" + view;
 
+      fs.readFile(view, "utf8", function (err, data) {
+        if (err) {
+          return console.log(err);
+        }
+        var result = data.replace(
+          "@import 'theme-dark.css'",
+          "@import 'theme-light.css'"
+        );
+
+        fs.writeFile(view, result, "utf8", function (err) {
+          if (err) return console.log(err);
+        });
+      });
+    }
+  }
 }
 
 module.exports = { setSettings, readSettings };
