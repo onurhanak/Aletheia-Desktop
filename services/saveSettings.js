3280de4208a6fad6c86dc5321e4c056c41ca299c
Onurhan Ak <onurhanak@protonmail.com>
2022-08-18 20:22:25 +0300

    save before test

--- services/saveSettings.js
+++ services/saveSettings.js
@@ -15,6 +15,7 @@ function setSettings(mirror, theme) {
     } 
     if (theme != undefined) {
         jsonSettings['theme'] = theme
+        applyTheme(theme)
     }
     let data = JSON.stringify(jsonSettings)
     fs.writeFileSync(settingsPath, data);
@@ -24,6 +25,22 @@ function readSettings() {
     let rawSettings = fs.readFileSync(settingsPath);
     let jsonSettings = JSON.parse(rawSettings)
     return [jsonSettings['theme'],jsonSettings['mirror']]
+}
+
+function applyTheme(theme){
+    var views = ['index.css','library.css','search.css','settings.css']
+    if (theme=='dark'){
+        for (let view of views) {
+            var cssFile = fs.readFileSync(view);
+            var newCssFile = cssFile.replace("@import 'theme-light.css'","@import 'theme-dark.css'")
+            fs.writeFileSync(view,newCssFile)
+        }
+        
+
+    } else if (theme=='light') {
+
+    }
+
 }
 
 module.exports = { setSettings, readSettings };
